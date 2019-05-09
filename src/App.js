15e616fbb53f69bbdae5aee191f664b71fed146b
Steven S <33554212+cthrustev@users.noreply.github.com>
2019-05-09 20:43:32 +0300

    set up

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import React from 'react';
 // import { connect } from 'react-redux';
-// import { BrowserRouter, Route, Link } from "react-router-dom";
+import { BrowserRouter, Route, Link } from "react-router-dom";
 // import { AnimatedSwitch } from 'react-router-transition';
 
-
+import Steven from './views/steven.js';
+import Kaarel from './views/kaarel.js';
 
 class App extends React.Component {
   render() {
     return (
-      <div> 123 </div>
+      <BrowserRouter>
+        <Route path="/steven" exact component={Steven} />
+        <Route path="/kaarel" exact component={Kaarel} />
+      </BrowserRouter>
     )
   }
 };
---
+++ src/views/steven.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class Steven extends React.Component {
+  render() {
+    return (
+      <div className="container">
+        456
+      </div>
+    )
+  }
+};
+
+
+export default Steven;
